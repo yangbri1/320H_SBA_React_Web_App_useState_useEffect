62310e2f9ef15c7eaacff84afc03471f1e2b016c
brian <yangbri1@msu.edu>
2024-11-12 15:37:17 -0500

    wireframing & going to attempt to integration 2 different API's --- 1 for weather & 1 for air traffic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import axios from 'axios';
 
+function App() {
+  // const [count, setCount] = useState(0)
+
+  const API_KEY = "aa37cadefc1679cd6669b048020034d2";
+
+  const URL = "https://api.openweathermap.org/data/2.5/";
+
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Air Quality/Weather</h1>
+
+      {/* Search Bar */}
+
+      {/* Display Location */}
+
+      {/* Display Temperature */}
+
+      {/* Display Current Conditions */}
     </>
   )
 }
