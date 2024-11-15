5791d1b8f0874b9cb2dc4c4c8323129847c2d7b5
brian <yangbri1@msu.edu>
2024-11-15 01:31:13 -0500

    unsure why it's functioning half of the time -- will scrap whole code & restart from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   // const [count, setCount] = useState(0)
 
   // create state to hold weather forecast
-  // const [search, setSearch] = useState("");
-  // const [weather, setWeather] = useState({});
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
 
   // create state to carry weather data -- initial value to null (workaround irregular errors in DOM compared to setting to "")
-  const [weather, setWeather] = useState(null);
+  // const [weather, setWeather] = useState(null);
 
   // initialize Weather API key to const variable API_KEY
   const API_KEY = "aa37cadefc1679cd6669b048020034d2";
@@ -22,68 +22,83 @@ function App() {
   // let luckybtn; -- find out how many datapoints are in weather api
   // let current_location;
   // retrieve geo data from Weather API 
-  // const deploySearch = () => {
-  //   // console.log("Go ahead");
-  //   // console.log(search);
-  //   fetch(`${URL}weather?q=${search}&units=imperial&appid=${API_KEY}`)
-  //   // convert response into JSON object format
-  //   .then((response) => response.json())
-  //   // output results to DOM
-  //   .then((results) => {
-  //     // console.log(results); // display "results" obj in DOM
-  //     // set weather equivalent to results
-  //     setWeather(results);
-  //   });
-  //   //https://api.openweathermap.org/data/2.5/weather?q=flint&units=metric&appid=aa37cadefc1679cd6669b048020034d2
-  // };
-
-  // going to be an "async" fn to pair with "await" fetch whenever accessing DB
-  const getWeather = async(searchInput) => {
-    // alongside a try-catch block for any extraneous errors
+  async function getData(){
+    // console.log("Go ahead");
+    // console.log(search);
     try {
-      // initialize response to empty string
-      let response = "";
-      // if the nothing was typed on search bar
-      if(searchInput == ""){
-        // generate random integer between 0-200,000 as Openweather API supports over 200,000 cities
-        let luckyCity = Math.floor(Math.random() * 200001);
-        // re-initialize response to a random city in database
-        response = await axios.get(`${URL}weather?id=${luckyCity}&units=imperial&appid=${API_KEY}`);
-      }
-      // otherwise ...
-      else{
-        // fetch relative data from database pertaining to inputted city 
-        response = await axios.get(`${URL}weather?q=${search}&units=imperial&appid=${API_KEY}`);
-      }
-    
-      // parse out JSON obj from response
-      const data = await response.json(); // might be response.data.json() -- since response.data in axios usually show everything
-      // set "weather" state to previously accepted data
-      setWeather(data);
-      // if there were to be any errors ...
+      let response = await fetch(`${URL}weather?q=${search}&units=imperial&appid=${API_KEY}`);
+      let data = await response.json();
     } catch (err) {
-      // execute out any errors to console
       console.error(err);
     }
+
+    
+    
+    // fetch(`${URL}weather?q=${search}&units=imperial&appid=${API_KEY}`)
+    // // convert response into JSON object format
+    // .then((response) => response.json())
+    // // output results to DOM
+    // .then((results) => {
+    //   // console.log(results); // display "results" obj in DOM
+    //   // set weather equivalent to results
+    //   setWeather(results);
+    // });
+    //https://api.openweathermap.org/data/2.5/weather?q=flint&units=metric&appid=aa37cadefc1679cd6669b048020034d2
   };
 
-  // apply React useEffect() hook so it will run on first & not subsequent renders
+  // utilize useEffect() React hook to make API call
   useEffect(() => {
-    getWeather("");
-  }, []); // empty list/array dependency -- initial render runs once
+    // invoke getData() function
+    getData();
+  }, []);   // empty list/array [] for initial render only
+
+  // going to be an "async" fn to pair with "await" fetch whenever accessing DB
+  // const getWeather = async(searchInput) => {
+  //   // alongside a try-catch block for any extraneous errors
+  //   try {
+  //     // initialize response to empty string
+  //     let response = "";
+  //     // if the nothing was typed on search bar
+  //     if(searchInput == ""){
+  //       // generate random integer between 0-200,000 as Openweather API supports over 200,000 cities
+  //       let luckyCity = Math.floor(Math.random() * 200001);
+  //       // re-initialize response to a random city in database
+  //       response = await axios.get(`${URL}weather?id=${luckyCity}&units=imperial&appid=${API_KEY}`);
+  //     }
+  //     // otherwise ...
+  //     else{
+  //       // fetch relative data from database pertaining to inputted city 
+  //       response = await axios.get(`${URL}weather?q=${search}&units=imperial&appid=${API_KEY}`);
+  //     }
+    
+  //     // parse out JSON obj from response
+  //     const data = await response.json(); // might be response.data.json() -- since response.data in axios usually show everything
+  //     // set "weather" state to previously accepted data
+  //     setWeather(data);
+  //     // if there were to be any errors ...
+  //   } catch (err) {
+  //     // execute out any errors to console
+  //     console.error(err);
+  //   }
+  // };
+
+  // // apply React useEffect() hook so it will run on first & not subsequent renders
+  // useEffect(() => {
+  //   getWeather("");
+  // }, []); // empty list/array dependency -- initial render runs once
 
   return (
     <>
-      <h1> Air Quality/Weather -- Travel (trip: long/lat, price)</h1>
+      <h1> Weather Forecast </h1>
 
       {/* Search Bar */}
       <label id="search">
         <input type="text" placeholder="Please enter a city" onChange={(event) => { setSearch(event.target.value)}} />
-        <button onClick={getWeather}>Go</button>
+        <button onClick={getData}>Go</button>
       </label>
       <div className="weather">
         {/* Display Location -- accessing using dot notation in "results" obj*/}
-        <h3>{weather.name}, {weather.sys.country}</h3>
+        <h3>{weather.name} </h3>
         {/* Display Temperature */}
         <h4>{weather.main.temp}<sup> o</sup>F</h4>
         {/* Display Current Conditions */}
@@ -92,14 +107,14 @@ function App() {
         
       </div>
       
-      <div className="news"></div>
+      {/* <div className="news"></div> */}
 
       <div className="flights">
         {/* Display Timezone */}
         <h3>{weather.timezone} ms</h3>
       </div>
           
-      <div className="language"></div>
+      {/* <div className="language"></div> */}
       
     </>
   )
